93a57873f11e6a9f81bc4ff98e82fa8082242ba7
Vitória Oliveira <vit.oliveira2000@gmail.com>
2023-04-30 02:06:21 -0300

    Atualizando Classes

---
+++ workspace_eclipese/lab_jpa/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/Fornecedor.java
@@ -0,0 +1,18 @@
+package br.inatel.labs.labjpa.entity;
+
+import java.util.List;
+
+public class Fornecedor {
+	
+	private Long id;
+	
+	private List<Produto> listaProduto;
+	
+	private String razaoSocial;
+	
+	
+	
+
+
+
+}
---
+++ workspace_eclipese/lab_jpa/lab_jpa/src/main/java/br/inatel/labs/labjpa/entity/Produto.java
@@ -0,0 +1,14 @@
+package br.inatel.labs.labjpa.entity;
+
+import java.util.List;
+
+public class Produto {
+	
+	private Long id;
+	
+	private List<Fornecedor> listaFornecedor;
+	
+	private String descricao;
+	
+	
+}
